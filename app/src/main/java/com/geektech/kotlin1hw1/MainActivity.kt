fe0c4113378be7bb29925620befc81e7ffb89c8f
AbylovErmek <gskro705@gmail.com>
2021-10-14 17:36:41 +0600

    finish

--- app/src/main/java/com/geektech/kotlin1hw1/MainActivity.kt
+++ app/src/main/java/com/geektech/kotlin1hw1/MainActivity.kt
@@ -1,35 +1,52 @@
 package com.geektech.kotlin1hw1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.geektech.kotlin1hw1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        registerForResult()
+        setupListener()
+    }
 
-        val title = intent.getStringExtra("title2")
-        binding.etTitle.setText(title)
+    private fun registerForResult() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK)
+                    checkData(result.data)
+            }
+    }
 
-        binding.btnNext.setOnClickListener {
-            if (binding.etTitle.text.isEmpty()) {
-                binding.etTitle.error = "Ошибка, заполните поле"
-                Toast.makeText(this, "Ошибка, заполните поле", Toast.LENGTH_SHORT).show()
-            } else startActivity()
+    private fun checkData(data: Intent?) {
+        if (data != null) {
+            val text = data.getStringExtra("title")
+            binding.etTitle.setText(text)
         }
     }
 
-    private fun startActivity() {
+    private fun setupListener() {
+        binding.btnNext.setOnClickListener {
+            if (!binding.etTitle.text.isNullOrEmpty()) send()
+            else binding.etTitle.error = "Ошибка, пожалуйста заполните поле"
+        }
+    }
+
+    private fun send() {
         val intent = Intent(this, SecondScreenActivity::class.java)
         intent.putExtra("title", binding.etTitle.text.toString())
-        startActivity(intent)
+        resultLauncher.launch(intent)
     }
 }
--- app/src/main/java/com/geektech/kotlin1hw1/SecondScreenActivity.kt
+++ app/src/main/java/com/geektech/kotlin1hw1/SecondScreenActivity.kt
@@ -1,10 +1,10 @@
 package com.geektech.kotlin1hw1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
 import com.geektech.kotlin1hw1.databinding.ActivitySecondScreenBinding
 
 class SecondScreenActivity : AppCompatActivity() {
@@ -15,21 +15,26 @@ class SecondScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondScreenBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        checkIntent()
+        setupListener()
+    }
 
-        val title = intent.getStringExtra("title")
-        binding.etTitle2.setText(title)
-
+    private fun setupListener() {
         binding.btnNext2.setOnClickListener {
-            if (binding.etTitle2.text.isEmpty()) {
-                binding.etTitle2.error = "Ошибка, заполните поле"
-                Toast.makeText(this, "Ошибка, заполните поле", Toast.LENGTH_SHORT).show()
-            } else finishActivity()
+            if (!binding.etTitle2.text.isNullOrEmpty()) send()
+            else binding.etTitle2.error = "Ошибка, пожалуйста заполните поле"
         }
     }
 
-    private fun finishActivity() {
+    private fun send() {
         val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("title2", binding.etTitle2.text.toString())
-        startActivity(intent)
+        intent.putExtra("title", binding.etTitle2.text.toString())
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+
+    private fun checkIntent() {
+        val text: String? = intent.getStringExtra("title")
+        binding.etTitle2.setText(text)
     }
 }
